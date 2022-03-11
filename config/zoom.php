f64a82a6c70cdef24c2d2db7f3da921d84bed812
suainul2 <suainulovi@gmail.com>
2022-03-11 09:50:07 +0700

    fixing bug api key

--- config/zoom.php
+++ config/zoom.php
@@ -2,6 +2,5 @@
 
 return [
     'base_url' => env('ZOOM_URL', 'https://api.zoom.us/'),
-    'client_id' => env('ZOOM_CLIENT_ID', ''),
-    'client_secret' => env('ZOOM_CLIENT_SECRET', '')
+    'api_key' => env('ZOOM_API_KEY', ''),
 ];
